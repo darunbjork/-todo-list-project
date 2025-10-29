4e36307037f7ce12df3344ba96b8c7938ad73b9a
darunbjork <darunbjork@gmail.com>
2025-10-29 17:30:09 +0100

    Add the useState hook for our two boxes: the list of tasks and the text being typed

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
 import { useState } from "react";
 
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
+  const [tasks, setTasks] = useState([]);
+  const [newTaskText, setNewTaskText] = useState("");
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>To Do List</h1>
+      <p>Number of tasks with length method {tasks.length}</p>
+      Current input text: {newTaskText}
+    </div>
   );
 }
 
